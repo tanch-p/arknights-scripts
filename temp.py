f35abaca68c1f5840022f91e324d742c5d9eb932
Chor <97508567+tanch-p@users.noreply.github.com>
2024-03-22 09:43:55 +0800

    update tiles

--- temp.py
+++ temp.py
@@ -1,29 +1,35 @@
+from os import walk
+import pprint
 import os
 import json
+
+pp = pprint.PrettyPrinter(indent=4)
+
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 
-with open('characters.json', encoding='utf-8') as f:
-    char_table = json.load(f)
+# with open('characters.json', encoding='utf-8') as f:
+#     char_table = json.load(f)
 
-list = []
-for chara in char_table:
-    if not chara['subProfessionId'] in list:
-        list.append(chara['subProfessionId'])
+# list = []
+# for chara in char_table:
+#     if not chara['subProfessionId'] in list:
+#         list.append(chara['subProfessionId'])
 
-return_dict = [ele for ele in list]
+# return_dict = [ele for ele in list]
 
-with open('temp.json','w', encoding='utf-8') as f:
-    json.dump(return_dict, f, ensure_ascii=False, indent=4)
+# with open('temp.json','w', encoding='utf-8') as f:
+#     json.dump(return_dict, f, ensure_ascii=False, indent=4)
 
-enemy_database_path = os.path.join(
-    script_dir, "cn_data/zh_CN/gamedata/levels/enemydata/enemy_database.json"
-)
-with open(enemy_database_path, encoding="utf-8") as f:
-    enemy_database = json.load(f)
+# enemy_database_path = os.path.join(
+#     script_dir, "cn_data/zh_CN/gamedata/levels/enemydata/enemy_database.json"
+# )
+# with open(enemy_database_path, encoding="utf-8") as f:
+#     enemy_database = json.load(f)
 
 # for enemy in enemy_database['enemies']:
 #     key = enemy['Key']
 #     for stats in enemy['Value']:
 #         if stats['enemyData']['attributes']['epDamageResistance']['m_value'] != 0 or stats['enemyData']['attributes']['epResistance']['m_value'] != 0:
 #             print(key)
+
 
--- tiles.py
+++ tiles.py
@@ -1,3 +1,94 @@
+from os import walk
+import pprint
+import os
+import json
+
+pp = pprint.PrettyPrinter(indent=4)
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+
+def get_all_tiles():
+    f = []
+    for (dirpath, dirnames, filenames) in walk(os.path.join(script_dir, "cn_data/zh_CN/gamedata/levels/obt")):
+        for filename in filenames:
+            f.append(os.path.join(dirpath, filename))
+
+    tiles = []
+    for path in f:
+        with open(path, encoding="utf-8") as f:
+            stage_data = json.load(f)
+        mapTiles = stage_data['mapData']['tiles']
+
+        for tile in mapTiles:
+            if tile['tileKey'] == 'tile_flower':
+                print(path)
+            if tile['tileKey'] not in tiles:
+                tiles.append(tile['tileKey'])
+    # pp.pprint(tiles)
+
+
+ALL_TILES = ['tile_forbidden',
+             'tile_start',
+             'tile_wall',
+             'tile_road',
+             'tile_floor',
+             'tile_end',
+             'tile_healing',
+             'tile_telout',
+             'tile_telin',
+             'tile_flystart',
+             'tile_infection',
+             'tile_hole',
+             'tile_fence',
+             'tile_yinyang_wall',
+             'tile_yinyang_road',
+             'tile_deepwater',
+             'tile_smog',
+             'tile_fence_bound',
+             'tile_deepsea',
+             'tile_wooden_wall',
+             'tile_defup',
+             'tile_empty',
+             'tile_volspread',
+             'tile_bigforce',
+             'tile_rcm_crate',
+             'tile_volcano',
+             'tile_icetur_lb',
+             'tile_icestr',
+             'tile_icetur_rb',
+             'tile_icetur_rt',
+             'tile_icetur_lt',
+             'tile_rcm_operator',
+             'tile_corrosion',
+             'tile_mire',
+             'tile_reed',
+             'tile_grass',
+             'tile_creep',
+             'tile_gazebo',
+             'tile_defbreak',
+             'tile_pollution_road',
+             'tile_pollution_wall',
+             'tile_creepf',
+             'tile_yinyang_switch',
+             'tile_stairs',
+             'tile_passable_wall',
+             'tile_reedw',
+             'tile_reedf',
+             'tile_xbdpsea',
+             'tile_puddle',
+             'tile_steam',
+             'tile_corrosion_2',
+             'tile_flowerf',
+             'tile_flower']
+
+NORMAL_TILES = ['tile_forbidden',
+                'tile_start',
+                'tile_wall',
+                'tile_road',
+                'tile_floor',
+                'tile_end', 'tile_telout',
+                'tile_telin',
+                'tile_flystart', 'tile_fence', 'tile_fence_bound', 'tile_empty', 'tile_stairs', 'tile_passable_wall']
+
 SP_TILES = ["tile_hole",
             "tile_deepsea",
             "tile_grass",
