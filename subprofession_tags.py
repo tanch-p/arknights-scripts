13688a41bf2059968e6166ecfb793388baf31ea2
Chor <97508567+tanch-p@users.noreply.github.com>
2025-05-02 17:36:16 +0800

    fix sub profession tags.py comma issue

--- subprofession_tags.py
+++ subprofession_tags.py
@@ -18,29 +18,30 @@ def get_sub_profession_tags(char, id):
         tags.append("position_all")
     if char['subProfessionId'] in ["unyield", "musha"]:
         tags.append("no_healing")
-    if char['subProfessionId'] in ["reaper", "guardian","bard"]:
+    if char['subProfessionId'] in ["reaper", "guardian", "bard"]:
         tags.append("heal_self")
     if char['subProfessionId'] in ["skywalker"]:
         tags.append("block_flying")
-    if char['subProfessionId'] in ["guardian","bard"]:
+    if char['subProfessionId'] in ["guardian", "bard"]:
         tags.append("heal_ally")
-    if char['subProfessionId'] in ["splashcaster",'blastcaster', "stalker", "hammer", "reaper", "aoesniper", "reaperrange", "bombarder", "phalanx"]:
+    if char['subProfessionId'] in ["splashcaster", 'blastcaster', "stalker", "hammer", "reaper", "aoesniper", "reaperrange", "bombarder", "phalanx"]:
         tags.append("aoe")
     if char['subProfessionId'] in ["instructor"]:
-        tags.extend(["no_block_enemy","bonus_no_block_enemy","self_no_block_enemy"]) 
+        tags.extend(
+            ["no_block_enemy", "bonus_no_block_enemy", "self_no_block_enemy"])
     if char['subProfessionId'] in ["tactician", "agent", "lord",
-                                   "fastshot", "closerange", "aoesniper", "longrange", "reaperrange", "siegesniper", "loopshooter", "hunter"
+                                   "fastshot", "closerange", "aoesniper", "longrange", "reaperrange", "siegesniper", "loopshooter", "hunter",
                                    "shotprotector", "incantationmedic", "slower", "summoner", "underminer", "blessing", "ritualist", "corecaster",
-                                   "splashcaster", "blastcaster", "funnel", "phalanx", "mystic", "chain", "primcaster", "hookmaster", "geek", "traper","alchemist","skywalker"]:
+                                   "splashcaster", "blastcaster", "funnel", "phalanx", "mystic", "chain", "primcaster", "hookmaster", "geek", "traper", "alchemist", "skywalker"]:
         tags.append("target_air")
     if char['subProfessionId'] in ["charger", "pioneer", "bearer", "tactician", "agent",
-                                   "sword", "fearless", "lord", "centurion", "reaper", "instructor", "fighter", "musha", "librator", "crusher", "hammer"
-                                   "fastshot", "closerange", "aoesniper", "longrange", "reaperrange", "siegesniper", "bombarder", "loopshooter", "hunter"
-                                   "protector", "guardian", "shotprotector", "artsprotector", "duelist", "fortress", "unyield"
-                                   "craftsman", "executor", "stalker", "pusher", "hookmaster", "merchant", "geek", "dollkeeper", "traper","alchemist","skywalker","primprotector","primguard"]:
+                                   "sword", "fearless", "lord", "centurion", "reaper", "instructor", "fighter", "musha", "librator", "crusher", "hammer",
+                                   "fastshot", "closerange", "aoesniper", "longrange", "reaperrange", "siegesniper", "bombarder", "loopshooter", "hunter",
+                                   "protector", "guardian", "shotprotector", "artsprotector", "duelist", "fortress", "unyield",
+                                   "craftsman", "executor", "stalker", "pusher", "hookmaster", "merchant", "geek", "dollkeeper", "traper", "alchemist", "skywalker", "primprotector", "primguard"]:
         tags.append("phys")
     if char['subProfessionId'] in ["artsfghter", "artsprotector", "incantationmedic",
                                    "slower", "summoner", "underminer", "blessing", "ritualist",
-                                   "corecaster", "splashcaster", "blastcaster", "funnel", "phalanx", "mystic", "chain", "primcaster","soulcaster"]:
+                                   "corecaster", "splashcaster", "blastcaster", "funnel", "phalanx", "mystic", "chain", "primcaster", "soulcaster"]:
         tags.append("arts")
     return tags
