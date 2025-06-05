fdc0b225e6a30a890b7b720be89882b8699ae0c6
Chor <97508567+tanch-p@users.noreply.github.com>
2025-06-05 17:18:14 +0800

    add new chars 5 jun 2025

--- generate_map_files.py
+++ generate_map_files.py
@@ -1,7 +1,7 @@
 import os
 from os import walk
 import json
-from waves_new import get_waves_data
+from waves_new import get_bonus_counts
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 
@@ -71,9 +71,7 @@ STAGES_WITH_ENEMY_REF_TO_REPLACE = {'level_rogue4_b-4': 'level_rogue4_b-4-c',
 
 # for consumption in enemy routes
 folders = ['ro1','ro2', 'ro3', 'ro4']
-stages_to_ignore = ['level_rogue1_1-4', 'level_rogue1_3-8', 'level_rogue2_2-5',
-                    'level_rogue3_3-3', 'level_rogue3_ev-9',
-                    'level_rogue4_d-1', 'level_rogue4_d-2', 'level_rogue4_d-3', 'level_rogue4_b-7', 'level_rogue4_b-8']
+stages_to_ignore = []
 for folder in folders:
     files = []
     path = os.path.join(
@@ -87,7 +85,7 @@ for folder in folders:
     for stage_id in files:
         if stage_id.split(".")[0] in stages_to_ignore:
             continue
-        if not 'level_rogue1_1-1.json' in stage_id:
+        if not 'level_rogue3_1-3.json' in stage_id:
             continue
         if "r1" in stage_id or "r2" in stage_id:
             continue
@@ -100,8 +98,11 @@ for folder in folders:
         )
         with open(file_path, encoding="utf-8") as f:
             stage_data = json.load(f)
-        timeline = get_waves_data(
-            stage_data, stage_id.replace(".json", ""), log=True)
+        # Example usage:
+        analysis = get_bonus_counts(stage_data,stage_id)
+
+        # timeline = get_waves_data(
+        #     stage_data, stage_id.replace(".json", ""), log=True)
         # write_path = os.path.join(
         #     script_dir, 'ro_wave_timelines', folder, stage_id)
         # if timeline:
--- test_waves.py
+++ test_waves.py
@@ -8,55 +8,55 @@ pp = pprint.PrettyPrinter(indent=4)
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 
 test_cases = [
-    # {
-    #     "path": "ro2/level_rogue2_3-5.json",
-    #     'id': "level_rogue2_3-5",
-    #     "all_possible_enemy_count": [47],
-    #             "sp_count": [48],
-    #             "elite_sp_count": [47],
-    # },
-    # {
-    #     "path": "ro2/level_rogue2_1-1.json",
-    #             'id': "level_rogue2_1-1",
-    #             "all_possible_enemy_count": [22, 23, 24, 25, 26, 27, 29, 31],
-    #             "sp_count": [28, 30, 32],
-    #             "elite_sp_count": None,
-    # },
-    # {
-    #     "path": "ro1/level_rogue1_4-6.json",
-    #     'id': "level_rogue1_4-6",
-    #             "all_possible_enemy_count": [19, 20, 21, 22, 23],
-    #     "sp_count": [24],
-    #             "elite_sp_count": None,
-    # },
-    # {
-    #     "path": "ro3/level_rogue3_1-2.json",
-    #     'id': "level_rogue3_1-2",
-    #             "all_possible_enemy_count": [31],
-    #     "sp_count": [32],
-    #             "elite_sp_count": [32],
-    # },
-    #     {
-    #     "path": "ro3/level_rogue3_3-2.json",
-    #     'id': "level_rogue3_3-2",
-    #             "all_possible_enemy_count": [33,34],
-    #     "sp_count": [35],
-    #             "elite_sp_count": None,
-    # },
-    # {
-    #     "path": "ro4/level_rogue4_4-10.json",
-    #     'id': "level_rogue4_4-10",
-    #             "all_possible_enemy_count": [42],
-    #     "sp_count": [43],
-    #             "elite_sp_count": None,
-    # },
     {
-        "path": "ro4/level_rogue4_b-8.json",
-        'id': "level_rogue4_b-8",
-                "all_possible_enemy_count": [55],
-        "sp_count": None,
+        "path": "ro2/level_rogue2_3-5.json",
+        'id': "level_rogue2_3-5",
+        "all_possible_enemy_count": [47],
+                "sp_count": [48],
+                "elite_sp_count": [47],
+    },
+    {
+        "path": "ro2/level_rogue2_1-1.json",
+                'id': "level_rogue2_1-1",
+                "all_possible_enemy_count": [22, 23, 24, 25, 26, 27, 29, 31],
+                "sp_count": [28, 30, 32],
                 "elite_sp_count": None,
     },
+    {
+        "path": "ro1/level_rogue1_4-6.json",
+        'id': "level_rogue1_4-6",
+                "all_possible_enemy_count": [19, 20, 21, 22, 23],
+        "sp_count": [24],
+                "elite_sp_count": None,
+    },
+    {
+        "path": "ro3/level_rogue3_1-2.json",
+        'id': "level_rogue3_1-2",
+                "all_possible_enemy_count": [31],
+        "sp_count": [32],
+                "elite_sp_count": [32],
+    },
+        {
+        "path": "ro3/level_rogue3_3-2.json",
+        'id': "level_rogue3_3-2",
+                "all_possible_enemy_count": [33,34],
+        "sp_count": [35],
+                "elite_sp_count": None,
+    },
+    {
+        "path": "ro4/level_rogue4_4-10.json",
+        'id': "level_rogue4_4-10",
+                "all_possible_enemy_count": [42],
+        "sp_count": [43],
+                "elite_sp_count": None,
+    },
+    # {
+    #     "path": "ro4/level_rogue4_b-8.json",
+    #     'id': "level_rogue4_b-8",
+    #             "all_possible_enemy_count": [55],
+    #     "sp_count": None,
+    #             "elite_sp_count": None,
+    # },
 ]
 # level_rogue3_b-1-b
 # level_rogue3_1-2
