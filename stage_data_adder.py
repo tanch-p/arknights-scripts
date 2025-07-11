3df58e7bb68bea5074968edf3f4c3d0ba52d856a
Chor <97508567+tanch-p@users.noreply.github.com>
2025-07-11 17:22:56 +0800

    update

---
+++ stage_data_adder.py
@@ -0,0 +1,18 @@
+from update_stage_extrainfo import add_new_ro_stages
+
+"""
+    USE THIS TO UPDATE ALL YOUR STUFF FOR MAJOR UPDATES
+    MOST IMPORTANTLY, IT SHOULD NOT MODIFY CURRENT ITEMS IF THERE IS NO UPDATE
+    
+    STEPS TO UPDATE:
+    1. ad
+
+    
+    
+"""
+
+def main():
+    add_new_ro_stages()
+
+if __name__ == "__main__":
+    main()
